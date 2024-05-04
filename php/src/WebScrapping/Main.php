e6013beccf69f5289ab96af231834c6f7dfbf62b
Tiago L. <51212563+Meee3@users.noreply.github.com>
2024-05-04 09:55:55 -0300

    Falta adicionar o node Authors

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,22 +2,34 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use Chuva\Php\WebScrapping\Entity\Paper;
+
 /**
- * Runner for the Webscrapping exercice.
+ * Runner for the Webscrapping exercise.
  */
-class Main {
+class Main
+{
 
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void {
+  public static function run(): void
+  {
     $dom = new \DOMDocument('1.0', 'utf-8');
+    libxml_use_internal_errors(true);
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-
+    $papers = [];
     $data = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
     print_r($data);
-  }
 
+    $xPath = new \DOMXPath($dom);
+    $domNodesID = $xPath->query('//div[@class="volume-info"]');
+    $titleNodes = $xPath->query('//h4[@class="my-xs paper-title"]');
+    $typeNodes = $xPath->query('//div[@class="tags mr-sm"]');
+    echo "<pre>";
+    print_r($typeNodes);
+    echo "</pre>";
+  }
 }
