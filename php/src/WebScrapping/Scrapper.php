5a545affcbe540db8550fc3b634592feae4bc772
Tiago L. <51212563+Meee3@users.noreply.github.com>
2024-05-04 21:27:03 -0300

    Scrapper

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,6 +14,33 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
+
+    $xPath = new \DOMXPath($dom);
+    $domNodesID = $xPath->query('//div[@class="volume-info"]');
+    $titleNodes = $xPath->query('//h4[@class="my-xs paper-title"]');
+    $typeNodes = $xPath->query('//div[@class="tags mr-sm"]');
+    $authorsNodes = $xPath->query('//div[@class="authors"]');
+    $authorsNodes = $xPath->query('//div[@class="authors"]');
+    $authorsInstitutions = $xPath->query('//div[@class="authors"]/span');
+   /*  $authors = [];
+
+    $authorGroups = $xPath->query('//div[@class="authors"]');
+    foreach ($authorGroups as $group) {
+      $groupAuthors = [];
+      $authorNodes = $group->getElementsByTagName('span');
+      foreach ($authorNodes as $authorNode) {
+        $author = [
+          'Author' => $authorNode->textContent,
+          'Instituiton' => $authorNode->getAttribute('title')
+        ];
+        $groupAuthors[] = $author;
+      }
+      $authors[] = $groupAuthors;
+    }
+
+    print_r($authors);
+  } */
+  
     return [
       new Paper(
         123,
